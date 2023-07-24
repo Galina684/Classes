287e53592163efde1c6e4693b6b101eff03aec66
Galina684 <galina684@gmail.com>
2023-07-24 19:19:48 +0300

    Домашка по теме Объекты и классы

---
+++ src/Hw12/Main.java
@@ -0,0 +1,21 @@
+package Hw12;
+
+import Hw12.Author;
+import Hw12.Book;
+
+public class Main {
+    public static void main(String[] args) {
+        Book onegin = new Book("Евгений Онегин", 1832);
+        Book godunov = new Book("Борис Годунов", 1825);
+        Author pushkin = new Author("Александр Пушкин");
+        System.out.println(pushkin.getName());
+        System.out.println(onegin.getTitle());
+        System.out.println(onegin.getYear());
+        onegin.setYear(1831);
+        System.out.println(onegin.getYear());
+        System.out.println(pushkin.getName());
+        System.out.println(godunov.getTitle());
+        System.out.println(godunov.getYear());
+
+    }
+}
