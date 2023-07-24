91d4e9a1ffe06b46b057e262f70e34b98e0b6ad1
Galina684 <galina684@gmail.com>
2023-07-24 20:46:24 +0300

    Исправление

--- src/Hw12/Book.java
+++ src/Hw12/Book.java
@@ -1,12 +1,19 @@
 package Hw12;
 
-public class Book {
+public class Book extends Author {
+    private  String name;
    private String title;
    private int year;
-   public Book(String title, int year){
+   public Book(String name, String title, int year){
+       super(name);
        this.title = title;
        this.year = year;
    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
 
     public String getTitle() {
         return title;
--- src/Hw12/Main.java
+++ src/Hw12/Main.java
@@ -5,8 +5,8 @@ import Hw12.Book;
 
 public class Main {
     public static void main(String[] args) {
-        Book onegin = new Book("Евгений Онегин", 1832);
-        Book godunov = new Book("Борис Годунов", 1825);
+        Book onegin = new Book("Александр Пушкин","Евгений Онегин", 1832);
+        Book godunov = new Book("Александр Пушкин", "Борис Годунов", 1825);
         Author pushkin = new Author("Александр Пушкин");
         System.out.println(pushkin.getName());
         System.out.println(onegin.getTitle());
