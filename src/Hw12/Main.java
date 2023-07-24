3212034b9bec0fac04730682e567fda0a9cdc805
Galina684 <galina684@gmail.com>
2023-07-24 20:58:33 +0300

    Исправление

--- src/Hw12/Main.java
+++ src/Hw12/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Book onegin = new Book("Александр Пушкин","Евгений Онегин", 1832);
         Book godunov = new Book("Александр Пушкин", "Борис Годунов", 1825);
+        Book dogsHeart = new Book ("Михаил Булгаков", "Собачье сердце", 1925);
         Author pushkin = new Author("Александр Пушкин");
+        Author bulgakov = new Author("Михаил Булгаков");
         System.out.println(pushkin.getName());
         System.out.println(onegin.getTitle());
         System.out.println(onegin.getYear());
@@ -16,6 +18,9 @@ public class Main {
         System.out.println(pushkin.getName());
         System.out.println(godunov.getTitle());
         System.out.println(godunov.getYear());
+        System.out.println(bulgakov.getName());
+        System.out.println(dogsHeart.getTitle());
+        System.out.println(dogsHeart.getYear());
 
     }
 }
